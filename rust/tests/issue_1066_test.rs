912373cb791e5fcb8a6560bb183a5cb3652e0c97
marcio.andrade <marcio.andrade@alphabot.com.br>
2025-04-10 12:51:48 -0300

    Update issue number.

--- rust/tests/issue_1066_test.rs
+++ rust/tests/issue_1066_test.rs
@@ -1,6 +1,6 @@
-use issue_1060::{
-    issue_1060_codec,
-    issue_1060_codec::{Issue1060Decoder, Issue1060Encoder},
+use issue_1066::{
+    issue_1066_codec,
+    issue_1066_codec::{Issue1066Decoder, Issue1066Encoder},
     *,
 };
 
@@ -9,7 +9,7 @@ fn decode_optional_primitive_field() {
     let mut buffer = vec![0u8; 256];
 
     // Create a message with `field` set.
-    let mut encoder = Issue1060Encoder::default().wrap(
+    let mut encoder = Issue1066Encoder::default().wrap(
         WriteBuf::new(&mut buffer),
         message_header_codec::ENCODED_LENGTH,
     );
@@ -20,20 +20,20 @@ fn decode_optional_primitive_field() {
 
     // Create a decoder for the message, but stating that the version is 1, instead of 2
     // which is the acting version for `field`. Thus, `field()` must return `None`.
-    let decoder = Issue1060Decoder::default().wrap(
+    let decoder = Issue1066Decoder::default().wrap(
         ReadBuf::new(&mut buffer),
         message_header_codec::ENCODED_LENGTH,
-        issue_1060_codec::SBE_BLOCK_LENGTH,
+        issue_1066_codec::SBE_BLOCK_LENGTH,
         1,
     );
     assert!(decoder.field().is_none());
 
     // Create a decoder for the message, but stating that the version is 1, instead of 2
     // which is the acting version for `field`. Thus, `field()` must return `None`.
-    let decoder = Issue1060Decoder::default().wrap(
+    let decoder = Issue1066Decoder::default().wrap(
         ReadBuf::new(&mut buffer),
         message_header_codec::ENCODED_LENGTH,
-        issue_1060_codec::SBE_BLOCK_LENGTH,
+        issue_1066_codec::SBE_BLOCK_LENGTH,
         2,
     );
     assert_eq!(decoder.field(), Some(10));
