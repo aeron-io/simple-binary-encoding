93ea6ec52a1e8d7fa2ccb30440680723a27a0484
Michael Ward <mward@drw.com>
2026-02-15 15:09:40 -0600

    [Rust] updated some function to "const"

--- sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/rust/MessageCoderDef.java
+++ sbe-tool/src/main/java/uk/co/real_logic/sbe/generation/rust/MessageCoderDef.java
@@ -83,7 +83,7 @@ class MessageCoderDef implements RustGenerator.ParentDef
         appendWrapFn(sb);
 
         indent(sb, 2, "#[inline]\n");
-        indent(sb, 2, "pub fn encoded_length(&self) -> usize {\n");
+        indent(sb, 2, "pub const fn encoded_length(&self) -> usize {\n");
         indent(sb, 3, "self.limit - self.offset\n");
         indent(sb, 2, "}\n\n");
 
